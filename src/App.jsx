b1ca7fac9fd5e44e9bb8467423b66213f8b29774
HelloHello1020 <hellologisticsthailand@gmail.com>
2025-09-07 11:14:25 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+  const [tasks, setTasks] = useState(["Go to my sister's friend's university campus in New York City", "Buy groceries", "Finish my React project", "Read a book", "Exercise for 30 minutes", "Go to my sister's friend's university campus in New York City", "Buy groceries", "Finish my React project", "Read a book", "Exercise for 30 minutes", "Go to my sister's friend's university campus in New York City", "Buy groceries", "Finish my React project", "Read a book", "Exercise for 30 minutes", "Go to my sister's friend's university campus in New York City", "Buy groceries", "Finish my React project", "Read a book", "Exercise for 30 minutes"])
+  const [doneTasks, setDoneTasks] = useState([""])
+
+  function deleteTask(index) {
+    const newTasks = tasks.filter((_, i) => i !== index)
+    setTasks(newTasks)
+  }
+
+  function doneTask(index) {
+    const taskToMove = tasks[index]
+    setDoneTasks([...doneTasks, taskToMove])
+    deleteTask(index)
+  }
+
+  return (
+    <div className="app">
+      <header className="header">
+        <h1 className="web-title">Me-ToDo</h1>
+      </header>
+
+      <main className="main">
+        <h1 className="list-title">To-Do List</h1>
+        <div className="btn-list">
+          <button className="add-btn">Add Task</button>
+          <button className="done-list-btn">Done List</button>
+        </div>
+        <ol className="tasks-list">
+          {tasks.map((task, index) => (
+            <div className="task-container" key={index}>
+                <li key={index} className="task">{task}</li>
+                <button className="delete-btn" onClick={() => deleteTask(index)}>Delete</button>
+                <button className="done-btn" onClick={() => doneTask(index)}>Done</button>
+            </div>
+          ))}
+        </ol>
+      </main>
+    </div>
+  )
+}
+
+export default App
