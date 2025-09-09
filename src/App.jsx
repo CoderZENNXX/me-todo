b91001769abc632d2de0a4074f4d4b3b62fffde2
CoderZENNXX <brocodestudent@gmail.com>
2025-09-09 19:40:05 +0700

    Project V2

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,40 @@ import './App.css'
 import { useState } from 'react'
 
 function App() {
-  const [tasks, setTasks] = useState(["Go to my sister's friend's university campus in New York City", "Buy groceries", "Finish my React project", "Read a book", "Exercise for 30 minutes", "Go to my sister's friend's university campus in New York City", "Buy groceries", "Finish my React project", "Read a book", "Exercise for 30 minutes", "Go to my sister's friend's university campus in New York City", "Buy groceries", "Finish my React project", "Read a book", "Exercise for 30 minutes", "Go to my sister's friend's university campus in New York City", "Buy groceries", "Finish my React project", "Read a book", "Exercise for 30 minutes", "Go to my sister's friend's university campus in New York City", "Buy groceries", "Finish my React project", "Read a book", "Exercise for 30 minutes", "Go to my sister's friend's university campus in New York City", "Buy groceries", "Finish my React project", "Read a book", "Exercise for 30 minutes", "Go to my sister's friend's university campus in New York City", "Buy groceries", "Finish my React project", "Read a book", "Exercise for 30 minutes", "Go to my sister's friend's university campus in New York City", "Buy groceries", "Finish my React project", "Read a book", "Exercise for 30 minutes", "Go to my sister's friend's university campus in New York City", "Buy groceries", "Finish my React project", "Read a book", "Exercise for 30 minutes", "Go to my sister's friend's university campus in New York City", "Buy groceries", "Finish my React project", "Read a book", "Exercise for 30 minutes", "Go to my sister's friend's university campus in New York City", "Buy groceries", "Finish my React project", "Read a book", "Exercise for 30 minutes", "Go to my sister's friend's university campus in New York City", "Buy groceries", "Finish my React project", "Read a book", "Exercise for 30 minutes", "Go to my sister's friend's university campus in New York City", "Buy groceries", "Finish my React project", "Read a book", "Exercise for 30 minutes", "Go to my sister's friend's university campus in New York City", "Buy groceries", "Finish my React project", "Read a book", "Exercise for 30 minutes", "Go to my sister's friend's university campus in New York City", "Buy groceries", "Finish my React project", "Read a book", "Exercise for 30 minutes"])
-  const [doneTasks, setDoneTasks] = useState([""])
+  const [tasks, setTasks] = useState(["Go to my sister's friend's university campus in New York City", "Buy groceries", "Finish my React project", "Read a book", "Exercise for 30 minutes"])
+  const [doneTasks, setDoneTasks] = useState([])
+  const [showDone, setShowDone] = useState(false)
+
+  function addTask(e) {
+    if (e.target.value.trim() !== "") {
+      setTasks([...tasks, e.target.value])
+    }
+    e.target.value = ""
+  }
 
   function deleteTask(index) {
-    const newTasks = tasks.filter((_, i) => i !== index)
-    setTasks(newTasks)
+    setTasks(tasks.filter((_, i) => i !== index))
+  }
+
+  function deleteDoneTask(index) {
+    setDoneTasks(doneTasks.filter((_, i) => i !== index))
   }
 
   function doneTask(index) {
     const taskToMove = tasks[index]
     setDoneTasks([...doneTasks, taskToMove])
     deleteTask(index)
+  }
+
+  function unDoneTask(index) {
+    const taskToMove = doneTasks[index]
+    setTasks([...tasks, taskToMove])
+    deleteDoneTask(index)
+  }
+
+  function toggleShowDone(e) {
+    setShowDone(!showDone)
+    e.target.textContent = showDone ? "Show Done Tasks" : "Hide Done Tasks"
   }
 
   return (
@@ -25,18 +47,36 @@ function App() {
       <main className="main">
         <h1 className="list-title">To-Do List</h1>
         <div className="menu">
-          <input type="text" className="task-input" placeholder="Enter a task" />
-          <button className="done-list-btn">Show Done</button>
+          <input type="text" className="task-input" placeholder="Enter a task" 
+            onKeyDown={(e) => e.key === "Enter" && addTask(e, e.target.value)}
+          />
+          <button className="done-list-btn" onClick={(e) => toggleShowDone(e)}>Show Done Tasks</button>
         </div>
-        <ol className="tasks-list">
-          {tasks.map((task, index) => (
-            <div className="task-container" key={index}>
-                <li key={index} className="task">{task}</li>
-                <button className="delete-btn" onClick={() => deleteTask(index)}>Delete</button>
-                <button className="done-btn" onClick={() => doneTask(index)}>Done</button>
-            </div>
-          ))}
-        </ol>
+        {
+          showDone ? (
+            <ol className="tasks-list">
+              {doneTasks.map((task, index) => (
+                <div className="task-container" key={index}>
+                    <li key={index} className="task">{task}</li>
+                    <button className="delete-btn" onClick={() => deleteDoneTask(index)}>Remove</button>
+                    <button className="done-btn" onClick={() => unDoneTask(index)}>Undone</button>
+                </div>
+              ))}
+            </ol>
+          ) : 
+          (
+            <ol className="tasks-list">
+              {tasks.map((task, index) => (
+                <div className="task-container" key={index}>
+                    <li key={index} className="task">{task}</li>
+                    <button className="delete-btn" onClick={() => deleteTask(index)}>Remove</button>
+                    <button className="done-btn" onClick={() => doneTask(index)}>Done</button>
+                </div>
+              ))}
+            </ol>
+          )
+        }
+        
       </main>
     </div>
   )
